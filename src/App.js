6e6d775fed6770b406efac4349337a9e4388ff92
jaskaran Kainth <jaskarankainth@hotmail.com>
2022-03-04 02:10:02 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -5,31 +5,30 @@
 // imports and API to get data on weather worldwife
 import React, {useState} from 'react';
 const api = {
-  key: "V17b8794dab4ee236b9e5ef49e867b4d6",
+  key: "c3fe9f50ef1be973bda0186b00f4a643",
   base: "https://api.openweathermap.org/data/2.5/"
 }
 
 //apps main function program
+
 function App() {
   const [query, setQuery] = useState('');
   const [weather, setWeather] = useState({});
 
-  //search function for the app
   const search = evt => {
-    if(evt.key === "enter") {
+    if (evt.key === "Enter") {
       fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
         .then(res => res.json())
         .then(result => {
-          setWeather(result)
+          setWeather(result);
           setQuery('');
           console.log(result);
         });
     }
   }
 
-// dates set alongside function to grab dates
   const dateBuilder = (d) => {
-    let months = ["January", "Feburary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
     let day = days[d.getDay()];
@@ -40,36 +39,30 @@ function App() {
     return `${day} ${date} ${month} ${year}`
   }
 
-
-  //main apps information display
   return (
-    <div className = {(typeof weather.main != "undefined")
-      ? ((weather.main.temp > 16) 
-        ? 'app warm'
-        : 'app') 
-        : 'app'}>
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
       <main>
-        <div classname = "search-box">
+        <div className="search-box">
           <input 
-          type = "text"
-          classname = "search-bar"
-          placeholder = "search..."
-          onChange={e => setQuery(e.target.value)}
-          value = {query}
-          onKeyPress = {search}
-            />
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
         </div>
-          {(typeof weather.main != "undefined") ? (
-            <div>
-          <div classname = "location-box">
-            <div classname = "location"> {weather.name}, {weather.sys.country}</div>
-              <div classname = "date">{dateBuilder(new Date())}</div>
+        {(typeof weather.main != "undefined") ? (
+        <div>
+          <div className="location-box">
+            <div className="location">{weather.name}, {weather.sys.country}</div>
+            <div className="date">{dateBuilder(new Date())}</div>
           </div>
-          <div classname = "weather-box">
-          <div classname = "temp">
-            {Math.round(weather.main.temp)}°c
-          </div>
-          <div classname = "weather">{weather.weather[0].main}</div>
+          <div className="weather-box">
+            <div className="temp">
+              {Math.round(weather.main.temp)}°c
+            </div>
+            <div className="weather">{weather.weather[0].main}</div>
           </div>
         </div>
         ) : ('')}
